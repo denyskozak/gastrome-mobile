4d05ff5d9a1c29dba38474d10a7b360ac101a83f
d.kozak <d.kozak@autodoc.eu>
2025-12-27 08:53:29 +0000

    chore: changes

--- src/styles/themes.js
+++ src/styles/themes.js
@@ -9,24 +9,11 @@ export const themes = {
       white: '#FFFFFF',
       red: '#BC7558',
       backgroundColor: '#000000',
-      backgroundColorLowOpacity: '#000000',
+      backgroundColorLowOpacity: '#000000CC',
       grey: '#BAA79D',
     },
   },
-  latte: {
-    id: 'latte',
-    label: 'Latte',
-    colors: {
-      primary: '#BC9A75',
-      black: '#32231A',
-      second: '#FBECE9',
-      white: '#FFFFFF',
-      red: '#BC7558',
-      backgroundColor: '#F8DFDB',
-      backgroundColorLowOpacity: '#F8DFDB99',
-      grey: '#D8C9C0',
-    },
-  },
+
   forest: {
     id: 'forest',
     label: 'Forest',
@@ -37,12 +24,43 @@ export const themes = {
       white: '#FFFFFF',
       red: '#C75C5C',
       backgroundColor: '#1D2A24',
-      backgroundColorLowOpacity: '#1D2A2488',
+      backgroundColorLowOpacity: '#1D2A24CC',
       grey: '#7D8D85',
+    },
+  },
+
+  obsidian: {
+    id: 'obsidian',
+    label: 'Obsidian',
+    colors: {
+      primary: '#4DA3FF',
+      black: '#0B0F14',
+      second: '#101621',
+      white: '#FFFFFF',
+      red: '#E06C75',
+      backgroundColor: '#0B0F14',
+      backgroundColorLowOpacity: '#0B0F14CC',
+      grey: '#6B7280',
+    },
+  },
+
+  ember: {
+    id: 'ember',
+    label: 'Ember',
+    colors: {
+      primary: '#FF8C42',
+      black: '#1A0F0A',
+      second: '#24140D',
+      white: '#FFFFFF',
+      red: '#E0573E',
+      backgroundColor: '#1A0F0A',
+      backgroundColorLowOpacity: '#1A0F0ACC',
+      grey: '#9A7B6A',
     },
   },
 };
 
 export const defaultThemeId = 'midnight';
 
-export const getThemeById = (id = defaultThemeId) => themes[id] || themes[defaultThemeId];
+export const getThemeById = (id = defaultThemeId) =>
+    themes[id] || themes[defaultThemeId];
